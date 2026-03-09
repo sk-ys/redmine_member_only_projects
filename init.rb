d3f3ea09301163f16e6a3ba8856fb977261a0dfa
sk-ys <yskrmt@gmail.com>
2026-03-10 08:23:14 +0900

    Add user preference patch

--- init.rb
+++ init.rb
@@ -1,6 +1,7 @@
 $LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__) + '/lib')
 require 'member_only_projects/patches/project_patch'
 require 'member_only_projects/patches/user_patch'
+require 'member_only_projects/patches/user_preference_patch'
 require 'member_only_projects/user_flag'
 require 'member_only_projects/hooks'
 
@@ -11,6 +12,3 @@ Redmine::Plugin.register :redmine_member_only_projects do
   version     '0.1.0'
   requires_redmine version_or_higher: '6.0.0'
 end
-
-# Allow the member_only_projects preference to be set via the standard pref[] form params
-UserPreference.safe_attributes 'member_only_projects'
